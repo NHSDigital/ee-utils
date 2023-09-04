8d419eba1e7c57df5a6ccb8e0aada506acfa2983
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-09-04 11:28:30 +0100

    added exports to .d.ts

--- src/s3Utilities.d.ts
+++ src/s3Utilities.d.ts
@@ -4,3 +4,15 @@ export declare const writeToS3HandleErrors = async (
   filename: string,
   isJson: boolean = true
 ): Promise<void> => {};
+
+export declare const outputToS3 = async (
+  body: string | object,
+  bucket: string,
+  key: string,
+  isJson: boolean
+): Promise<void> => {};
+
+export declare const getParsedJSONFromS3 = async (
+  bucket: string,
+  file: string
+): Promise<object> => {};
