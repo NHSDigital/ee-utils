d8d0ba4fd6b7bf89cc1f3042d01491690c80fe2b
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-09-13 12:11:33 +0100

    added type

--- src/octokit.d.ts
+++ src/octokit.d.ts
@@ -19,3 +19,16 @@ export declare const getAllRepositoriesInOrganisation = async (
   octokit: Octokit,
   organisationName: string
 ): Promise<Array<string>> => {};
+
+export type OrgInfo = {
+  orgName: string;
+  appId: string;
+  privateKey: string;
+  installationId: string;
+  elevatedAppId: string;
+  elevatedPrivateKey: string;
+  elevatedInstallationId: string;
+  repoMetaDataAppId: string;
+  repoMetaDataPrivateKey: string;
+  repoMetaDataInstallationId: string;
+};
