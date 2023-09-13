cfa55f53ceabd205206d13df803d6e8716f2607e
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-09-13 13:51:07 +0100

    added filenames

--- src/filenames.d.ts
+++ src/filenames.d.ts
@@ -1,2 +1,8 @@
 export declare const GITHUB_ACTION_MINUTES: string;
 export declare const UNIQUE_CONTRIBUTORS: string;
+export declare const DEPENDABOT_METRICS: string;
+export declare const HIERARCHIES: string;
+export declare const METRICS: string;
+export declare const NHSDIGITAL_REPOS: string;
+export declare const SONARCLOUD_METRICS: string;
+export declare const REPO_BRANCH_PROTECTION: string;
