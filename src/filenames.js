806fe679d425ae67dc628a7611a7abe59386ca11
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-09-14 12:16:34 +0100

    added filename

--- src/filenames.js
+++ src/filenames.js
@@ -6,3 +6,4 @@ export const SONARCLOUD_METRICS = "sonarcloud_metrics.json";
 export const REPO_BRANCH_PROTECTION = "repo_branch_protection.json";
 export const GITHUB_ACTION_MINUTES = "github_action_minutes.json";
 export const UNIQUE_CONTRIBUTORS = "unique_contributors.json";
+export const REPO_INFORMATION = "repo_information.json";
