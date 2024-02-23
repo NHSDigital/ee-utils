c5ac3677d77972601437cff5ee2aa77212f99e42
amaanibn-nasar1-nhs <amaan.ibn-nasar1@nhs.net>
2024-02-23 13:35:50 +0000

    Fixed build issue by upgrading packages

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
-  testPathIgnorePatterns: ["/node_modules/"],
+  testPathIgnorePatterns: ["/node_modules/", "/lib/s"],
   moduleFileExtensions: ["js", "json", "ts"],
 };
 export default config;
