e17256ba136e8fd268832148a1b8fe7b8e6c80a9
amaanibn-nasar1-nhs <amaan.ibn-nasar1@nhs.net>
2024-02-27 14:44:00 +0000

    Changed hierarchy schema to allow partial hiearchies

--- src/mongodb/__tests__/hierarchySchemas.test.ts
+++ src/mongodb/__tests__/hierarchySchemas.test.ts
@@ -2,27 +2,150 @@ import { HierarchyModel } from "../hierarchySchemas";
 
 describe("HierarchySchema", () => {
   describe("validation", () => {
-    const validHierarchy = {
-      repo: "repo",
-    };
-    it("should default hierarchy to Unallocated", () => {
+    it("should succeed for only directorate hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+      };
       const validModel = new HierarchyModel(validHierarchy);
 
-      expect(validModel.validateSync()).toBeUndefined();
-      expect(validModel.repo).toEqual("repo");
-      expect(validModel.directorate).toEqual("Unallocated");
-      expect(validModel.function_name).toEqual("Unallocated");
-      expect(validModel.subdirectorate).toEqual("Unallocated");
-      expect(validModel.area).toEqual("Unallocated");
-      expect(validModel.service).toEqual("Unallocated");
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
     });
-    it("should fail if the repo is not present", () => {
-      const invalidHierarchy = {};
-      const invalidModel = new HierarchyModel(invalidHierarchy);
+    it("should succeed for function hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+        function_name: "Function 1",
+      };
+      const validModel = new HierarchyModel(validHierarchy);
 
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
+    });
+    it("should succeed for subdirectorate hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+        function_name: "Function 1",
+        subdirectorate: "Subdirectorate 1",
+      };
+      const validModel = new HierarchyModel(validHierarchy);
+
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
+    });
+    it("should succeed for area hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+        function_name: "Function 1",
+        subdirectorate: "Subdirectorate 1",
+        area: "Area 1",
+      };
+      const validModel = new HierarchyModel(validHierarchy);
+
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
+    });
+    it("should succeed for service hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+        function_name: "Function 1",
+        subdirectorate: "Subdirectorate 1",
+        area: "Area 1",
+        service: "Service 1",
+      };
+      const validModel = new HierarchyModel(validHierarchy);
+
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
+    });
+    it("should succeed for repo hierarchy", () => {
+      const validHierarchy = {
+        directorate: "Directorate 1",
+        function_name: "Function 1",
+        subdirectorate: "Subdirectorate 1",
+        area: "Area 1",
+        service: "Service 1",
+        repo: "Repo 1",
+      };
+      const validModel = new HierarchyModel(validHierarchy);
+
+      const error = validModel.validateSync();
+      expect(error).toBeUndefined();
+    });
+    it.each([
+      {
+        description: "should fail if the directorate is not present",
+        hierarchy: {},
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+        },
+      },
+      {
+        description:
+          "should fail if directorate is not present and function is",
+        hierarchy: {
+          function_name: "function",
+        },
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+        },
+      },
+      {
+        description:
+          "should fail if directorate and function are not defined but subdirectorate is",
+        hierarchy: {
+          subdirectorate: "subdirectorate",
+        },
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+          function_name: "Path `function_name` is required.",
+        },
+      },
+      {
+        description:
+          "should fail if directorate, function and subdirectorate not defined and area is",
+        hierarchy: {
+          area: "area",
+        },
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+          function_name: "Path `function_name` is required.",
+          subdirectorate: "Path `subdirectorate` is required.",
+        },
+      },
+      {
+        description:
+          "should fail if directorate, function, subdirectorate, area not defined but service is",
+        hierarchy: {
+          service: "service",
+        },
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+          function_name: "Path `function_name` is required.",
+          subdirectorate: "Path `subdirectorate` is required.",
+          area: "Path `area` is required.",
+        },
+      },
+      {
+        description:
+          "should fails if directorate, function, subdirectorate, area, service not defined but repo is",
+        hierarchy: {
+          repo: "repo",
+        },
+        expectedErrors: {
+          directorate: "Path `directorate` is required.",
+          function_name: "Path `function_name` is required.",
+          subdirectorate: "Path `subdirectorate` is required.",
+          area: "Path `area` is required.",
+          service: "Path `service` is required.",
+        },
+      },
+    ])("$description", ({ hierarchy, expectedErrors }) => {
+      const invalidModel = new HierarchyModel(hierarchy);
       const error = invalidModel.validateSync();
       expect(error).toBeDefined();
-      expect(error?.errors.repo.message).toEqual("Path `repo` is required.");
+      Object.entries(expectedErrors).forEach(([field, message]) => {
+        expect(error?.errors[field].message).toEqual(message);
+      });
     });
   });
 });
