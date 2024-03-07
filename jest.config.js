75a068f3704a830fd97a2f07e5a65a52c02b8f7e
amaanibn-nasar1-nhs <amaan.ibn-nasar1@nhs.net>
2024-03-07 09:40:37 +0000

    Removed s

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
-  testPathIgnorePatterns: ["/node_modules/", "/lib/s"],
+  testPathIgnorePatterns: ["/node_modules/", "/lib/"],
   moduleFileExtensions: ["js", "json", "ts"],
 };
 export default config;
